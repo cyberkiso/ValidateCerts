ec3e73bb42d186fa86bbf826ad8e071445b1aa46
cyberkiso <33750054+cyberkiso@users.noreply.github.com>
2019-04-04 20:09:17 +0300

    add some sparkles

--- ValidateCerts.py
+++ ValidateCerts.py
@@ -8,89 +8,94 @@ import validators
 import aiohttp
 import os
 from aiohttp import ClientConnectorCertificateError
+import logging
 
-Sem = asyncio.Semaphore(os.cpu_count() * 2)
+
+log = logging.getLogger('')
+log.setLevel(logging.DEBUG)
+format = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+ch = logging.StreamHandler(sys.stdout)
+ch.setFormatter(format)
+log.addHandler(ch)
 
 
 def main(argv):
-    global Sem
     try:
-        opts, args = getopt.getopt(sys.argv[2:], "n:")
+        sem = asyncio.Semaphore(os.cpu_count() * 2)
+        opts, args = getopt.getopt(sys.argv[1:], "f:n:")
         for opt, arg in opts:
             if opt == '-n':
-                Sem = asyncio.Semaphore(int(arg))
-        inputfile: str = sys.argv[1]
-        file = open(inputfile, "r")
-        rawdata = file.readlines()
-        file.close()
+                sem = asyncio.Semaphore(int(arg))
+            if opt == '-f':
+                if len(arg) > 0:
+                    file_name = arg
+                else:
+                    raise getopt.GetoptError
+        input_file = file_name
+        with open(input_file, "r") as file:
+            raw_data = file.readlines()
         domains = set()
-        for domain in set(map(str.strip, rawdata)):
+        for domain in set(map(str.strip, raw_data)):
             if validators.domain(domain):
                 domains.add(domain)
-        print("Domains:", domains)
+        log.debug(f"Domains: {domains}")
         loop = asyncio.get_event_loop()
-        loop.run_until_complete(validate_certs(domains))
+        loop.run_until_complete(validate_certs(domains, sem))
         loop.close()
     except getopt.GetoptError:
-        print("Usage: ValidateCerts.py <inputfile> [-n <requests_limit>]")
-        sys.exit(2)
-    except IOError as error:
-        print("Error:", error)
+        log.error("Usage: ValidateCerts.py -f <inputfile> [-n <requests_limit>]")
     except IndexError:
-        print("Error: input file name is missing."
-              "Usage: ValidateCerts.py <inputfile>")
+        log.error("Error: input file name is missing."
+                  "Usage: ValidateCerts.py -f <inputfile>")
     except Exception as e:
-        print(e)
+        log.error(f"Exception: {e}")
 
 
-async def validate(domain):
+async def validate(domain, sem):
     from _ssl import SSLCertVerificationError
-    async with Sem:
+    async with sem:
         try:
-            print("Validate domain=", domain)
-            url = "https://" + domain + "/"
+            log.debug(f"Validate domain={domain}")
+            url = f"https://{domain}/"
             async with aiohttp.ClientSession() as session:
                 await session.get(url)
-            Sem.release()
+            sem.release()
             return domain
         except ClientConnectorCertificateError as e:
-            await session.close()
-            Sem.release()
+            sem.release()
             if type(e.certificate_error is SSLCertVerificationError):
                 return None
-            print("Validate error: domain=%s  exception=%s", domain, e)
-            return Exception("Validate error: domain=%s  exception=%s", domain, e), domain
+            log.debug(f"Validate error: domain={domain} exception={e}")
+            return \
+                Exception(f"Validate error: domain={domain} exception={e}"), \
+                domain
         except Exception as e:
-            await session.close()
-            Sem.release()
-            print("Validate error: domain=%s  exception=%s", domain, e)
-            return Exception("Validate error: domain=%s  exception=%s", domain, e), domain
+            sem.release()
+            log.debug(f"Validate error: domain={domain} exception={e}")
+            return \
+                Exception(f"Validate error: domain={domain} exception={e}"), \
+                domain
 
 
-async def validate_certs(domains, ):
+async def validate_certs(domains, sem):
     try:
-        tasks = [asyncio.ensure_future(
-            validate(domain)) for domain in domains]
+        tasks = [asyncio.ensure_future(validate(domain, sem)) for domain in domains]
         done = await asyncio.gather(*tasks)
-        validdomains = set()
+        valid_domains = set()
         for result in done:
             if result is not None and type(result) is not tuple:
-                print("Valid: ", result)
-                validdomains.add(result+"\n")
+                valid_domains.add(result + "\n")
                 domains.remove(result)
             elif type(result) is tuple:
-                print("Cannot get cert for domain=", result[1])
+                log.warning(f"Cannot get cert for domain={result[1]}")
                 domains.remove(result[1])
-        for domain in domains:
-            print("Unvalid: ", domain)
-        validfile = open("Valid", "w")
-        validfile.writelines(validdomains)
-        validfile.close()
-        unvalidfile = open("Unvalid", "w")
-        unvalidfile.writelines(domains)
-        unvalidfile.close()
+        with open("Valid", "w") as valid_file:
+            valid_file.writelines(valid_domains)
+        with open("Unvalid", "w") as unvalid_file:
+            unvalid_file.writelines(domains)
     except Exception as e:
-        print(e)
+        log.error(f"Exception: {e}")
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
